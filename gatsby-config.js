7585de40a2b7ed8bca105d99c7f17294a7643f18
Luke.Batchelor <batchelor.luke+git@gmail.com>
2018-10-28 15:15:32 +1100

    Adds icons, cleans up unused files

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,12 @@ module.exports = {
         name: 'face-folding',
         short_name: 'face-folding',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#f1f1f1',
+        theme_color: '#ffffff',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/trumpFoldIcon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
   ],
-}
+};
